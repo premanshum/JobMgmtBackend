240dcd77ffc57dab2aef9d0d43d3a673504fdf4c
premanshum <premanshu.mukherji@gmail.com>
2021-03-26 16:32:17 +0530

    Add project files.

---
+++ JobManagementApi/Authentication/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace JobManagementApi.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ JobManagementApi/Entities/LoginResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace JobManagementApi.Entities
+{
+    public class LoginResponse
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
